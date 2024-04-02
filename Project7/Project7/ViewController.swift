aaaf6e9ebac71fe79b13c5f04cb3192c0059efe7
thee-falcon <omar.makran.10@gmail.com>
2024-04-02 22:22:35 +0000

    is going on

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Omar Makran on 4/1/24.
+//  Copyright © 2024 Omar Makran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
